11bda0fc6216229f2e1709166aa2ec3371692626
TejShinde <shindetejal995@gmail.com>
2024-12-10 17:31:14 +0530

    Add project files.

---
+++ WindowsFormsApp2/StudentList.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class StudentList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lstStudents = new System.Windows.Forms.ListBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblList = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(337, 72);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(100, 20);
+            this.txtStudentName.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(302, 327);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(415, 327);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // lstStudents
+            // 
+            this.lstStudents.FormattingEnabled = true;
+            this.lstStudents.Location = new System.Drawing.Point(337, 163);
+            this.lstStudents.Name = "lstStudents";
+            this.lstStudents.Size = new System.Drawing.Size(120, 95);
+            this.lstStudents.TabIndex = 4;
+            this.lstStudents.SelectedIndexChanged += new System.EventHandler(this.lstStudents_SelectedIndexChanged);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(204, 79);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(103, 13);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Enter Student Name";
+            // 
+            // lblList
+            // 
+            this.lblList.AutoSize = true;
+            this.lblList.Location = new System.Drawing.Point(204, 192);
+            this.lblList.Name = "lblList";
+            this.lblList.Size = new System.Drawing.Size(63, 13);
+            this.lblList.TabIndex = 6;
+            this.lblList.Text = "Student List";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblList);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lstStudents);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtStudentName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ListBox lstStudents;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblList;
+    }
+}
+
---
+++ WindowsFormsApp2/StudentList.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class StudentList : Form
+    {
+        public StudentList()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            String stName= txtStudentName.Text.Trim();
+
+            //if(string.IsNullOrEmpty(stName))
+            //{
+            //    MessageBox.Show("Please enter student name");
+            //    return;
+            //}
+
+            //if(lstStudents.Items.Contains(stName))
+            //{
+            //    MessageBox.Show("This Student is already in the list");
+            //    return;
+            //}
+
+            if(stName== "")
+            {
+                MessageBox.Show("Please enter student name");
+                return;
+            }
+
+            bool isDuplicate = false;
+            for(int i=0;i<lstStudents.Items.Count;i++)
+            {
+                if (lstStudents.Items[i].ToString() == stName)
+                {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if(isDuplicate)
+            {
+                MessageBox.Show("This Student is already in the list");
+                return;
+            }
+
+            lstStudents.Items.Add(stName);
+            txtStudentName.Clear(); 
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstStudents.SelectedIndex == -1)
+            {
+
+                MessageBox.Show("Please select a student to remove");
+                return;
+            }
+            string SelectedItem = lstStudents.SelectedItem.ToString();
+            for (int i = 0; i < lstStudents.Items.Count; i++)
+            {
+                if (lstStudents.Items[i].ToString() == SelectedItem)
+                {
+                    lstStudents.Items.RemoveAt(i);
+                    break;
+                }
+                   
+            }
+        }
+
+        private void lstStudents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
